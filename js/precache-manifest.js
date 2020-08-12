bd1869a7ba98f27bf4ccd7b4d4cc6c04b55d0098
christoper.wang <christopher.wang@zoom.us>
2020-08-12 13:23:04 +0800

    demo

---
+++ js/precache-manifest.js
@@ -0,0 +1,6 @@
+
+  self.__precacheList = [
+    "https://rwgdev100.zoomdev.us:11099/pwa-cdn/static/js/main.js",
+    "https://rwgdev100.zoomdev.us:11099/pwa-cdn/static/css/main.css",
+    "/pwa/",
+  ]
